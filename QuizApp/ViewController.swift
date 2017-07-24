dcd26015194e5deffa2e636a6a5adfd20c784a0f
Nitish Dash <nitishdash@Nitishs-Mac.local>
2017-07-24 07:21:04 -0700

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Nitish Dash on 24/07/17.
+//  Copyright © 2017 Nitish Dash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
